875117546d06b84a6fc6e9b72e4e1434f8eaa57c
Ananya Priya <ananya.priya@intermesh.com>
2024-11-09 19:38:47 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const path = require("path");
+const connectDb = require("./config/connectDb");
+// config dot env file
+dotenv.config();
+
+//databse call
+connectDb();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: "*",
+    credentials: true,
+  })
+);
+
+//routes
+//user routes
+app.use("/api/v1/users", require("./routes/userRoute"));
+//transections routes
+app.use("/api/v1/transections", require("./routes/transectionRoutes"));
+
+app.get("/", (req, res) => {
+  res.send("API is up and running!!");
+});
+
+//port
+const PORT = 8080 || process.env.PORT;
+
+//listen server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
